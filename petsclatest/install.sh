80e123d2bd99f98d841cc42a099f59ce62adf603
David Hrbáč <david@hrbac.cz>
2022-11-23 15:11:45 +0100

    Petsc install test

--- petsclatest/install.sh
+++ petsclatest/install.sh
@@ -1,7 +1,1 @@
-apt update
-apt -y install petsc-dev && apt -y remove petsc-dev
-su - ubuntu
-git clone -b release https://gitlab.com/petsc/petsc.git petsc
-cd petsc/
-./configure
-make all check
+apt update; apt -y install petsc-dev && apt -y remove petsc-dev; su - ubuntu; git clone -b release https://gitlab.com/petsc/petsc.git petsc; cd petsc/; ./configure; make all check
